aed629096dca35c585c0afbc6c66aa99a7be0156
cloudy <87387633+CloudyDaKing@users.noreply.github.com>
2024-09-13 23:48:21 +0100

    Initial commit

---
+++ src/lib/components/navbar.svelte
@@ -0,0 +1,15 @@
+<script>
+    import sun from "$lib/assets/sun.svg";
+</script>
+
+<nav class="h-14 mx-5 mt-3 rounded-full bg-[#D9D9D9] relative text-xl font-inter md:text-2xl">
+    <a href="https://example.com" class="absolute transform top-1/2 -translate-y-1/2 ml-5">
+        <img src={sun} class="h-8 w-10" alt="sun" />
+    </a>
+
+    <div class="absolute right-5 top-1/2 transform -translate-y-1/2 text-sm md:text-base lg:text-lg">
+        <a href="#" class="mr-4 font-semibold">My Projects</a>
+        <a href="#" class="mr-4 font-semibold">About Me</a>
+        <a href="#" class="font-semibold mr-4">Contact</a>
+    </div>
+</nav>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,31 @@
+<script>
+  import github from "$lib/assets/github.svg";
+  import link from "$lib/assets/link.svg";
+  import mail from "$lib/assets/mail.svg";
+</script>
+
+
+<div class="flex flex-col justify-center items-center h-[calc(100vh-69px)]">
+
+
+  <h1 class="text-6xl font-semibold">
+    Cloudy
+  </h1>
+
+  <h2 class="font-semibold text-2xl text-center">
+    Full Stack Dev
+  </h2>
+
+  <div class="flex mt-2">
+    <a href="" >
+      <img src={github} class="h-8 w-8 mx-2" alt="github" />
+    </a>
+    <a href="" >
+      <img src={mail} class="h-8 w-8 mx-2" alt="github" />
+    </a>
+    <a href="" >
+      <img src={link} class="h-7 w-7 mx-2" alt="github" />
+    </a>
+  </div>
+
+</div>
